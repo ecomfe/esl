09cc189a379a2dd313c624d2bcdd7784ddb02a04
erik <erik168@163.com>
2013-03-08 22:12:33 +0800

    add module id

--- src/css.js
+++ src/css.js
@@ -1,4 +1,14 @@
-define( {
+/**
+ * ESL (Enterprise Standard Loader)
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ * 
+ * @file CSS Loader-Plugin
+ * @author errorrik(errorrik@gmail.com)
+ */
+
+// 构建环境暂不支持分析，为了能合并该plugin到loader里，
+// 只能暂时使用具名id
+define( 'css', {
     load: function ( resourceId, req, load, config ) {
         var link = document.createElement( 'link' );
         link.setAttribute( 'rel', 'stylesheet' );
@@ -6,7 +16,10 @@ define( {
         link.setAttribute( 'href', req.toUrl( resourceId ) );
 
         var parent = document.getElementsByTagName( 'head' )[ 0 ] || document.body;
-        parent.appendChild( link ) && ( parent = null );
+        parent.appendChild( link );
+
+        parent = null;
+        link = null;
 
         load( true );
     }
