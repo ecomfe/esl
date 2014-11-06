c54bd88502192facb0a80519c8168c8b24e7b508
erik <erik168@163.com>
2014-11-06 15:27:25 +0800

    remove unuseful variable

--- src/esl.js
+++ src/esl.js
@@ -282,14 +282,6 @@ var esl;
     }
 
     /**
-     * 尝试完成模块定义的定时器
-     *
-     * @inner
-     * @type {number}
-     */
-    var tryDefineTimeout;
-
-    /**
      * 定义模块
      *
      * @param {string=} id 模块标识
@@ -338,12 +330,6 @@ var esl;
 
         if (id) {
             modPreDefine(id, dependencies, factory);
-
-            // 在不远的未来尝试完成define
-            // define可能是在页面中某个地方调用，不一定是在独立的文件被require装载
-            if (tryDefineTimeout) {
-                clearTimeout(tryDefineTimeout);
-            }
         }
         else {
             // 纪录到共享变量中，在load或readystatechange中处理
