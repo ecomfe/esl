fcc144e253cdad7ccfaa3e25bc9d3fe6a0652902
erik <erik168@163.com>
2015-01-13 09:13:42 +0800

    we dont need min edition

--- tool/dist.sh
+++ tool/dist.sh
@@ -10,21 +10,21 @@ mkdir dist
 
 mkdir dist/src
 cp src/esl.js dist/src/esl.source.js
-node tool/rm-ignore.js -i dist/src/esl.source.js -o dist/src/esl.source.min.js
+# node tool/rm-ignore.js -i dist/src/esl.source.js -o dist/src/esl.source.min.js
 uglifyjs dist/src/esl.source.js -m -c -o dist/src/esl.js
-uglifyjs dist/src/esl.source.min.js -m -c -o dist/src/esl.min.js
+# uglifyjs dist/src/esl.source.min.js -m -c -o dist/src/esl.min.js
 uglifyjs src/js.js -m -c -o dist/src/js.js
 uglifyjs src/css.js -m -c -o dist/src/css.js
 
 gzip -kf dist/src/esl.js
-gzip -kf dist/src/esl.min.js
+# gzip -kf dist/src/esl.min.js
 
 
 cp -r test dist/test
-cp -r test dist/test-min
+# cp -r test dist/test-min
 
 rm dist/test/impl/esl/esl.js
 cp dist/src/esl.js dist/test/impl/esl/esl.js
 
-rm dist/test-min/impl/esl/esl.js
-cp dist/src/esl.min.js dist/test-min/impl/esl/esl.js
+# rm dist/test-min/impl/esl/esl.js
+# cp dist/src/esl.min.js dist/test-min/impl/esl/esl.js
