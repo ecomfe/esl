21c96e1e50e1aa8279fe5b124d3a888e0d3bf2c6
chris <wfsr@foxmail.com>
2015-10-28 13:50:24 +0800

    add test case for function name shim

--- test/shim/mixed/simpleIndex.js
+++ test/shim/mixed/simpleIndex.js
@@ -1,11 +1,13 @@
 
 define(function (require) {
     var b = require('shimMixedSB');
+    var g = require('shimMixedSG');
+
 
     return {
         name: "shim mixed simpleIndex",
         check: function () {
-            return b === shimMixedSBOther; 
+            return b === shimMixedSBOther && g === shimMixedSGOther;
         }
     };
 });
