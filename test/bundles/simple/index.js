37e2ae38649c1f72afc90898df9d6a5d3654b090
erik <erik168@163.com>
2017-07-18 17:09:11 +0800

    不合理的测试用例

--- test/bundles/simple/index.js
+++ test/bundles/simple/index.js
@@ -1,9 +1,9 @@
-define({ name: 'bundles/simple/index' })
+define('bundles/simple/index', { name: 'bundles/simple/index' })
 
 define( 'bundles/simple/plugin!resource', function () {
     return 'bundles/simple/plugin!resource';
 });
-    
+
 
 define( 'bundles/simple/cat', function () {
     return { name: 'bundles/simple/cat' }
