be41499985ebb48b4def2b3215361cff6381d10c
leeight <leeight@gmail.com>
2017-07-19 12:14:55 +0800

    Add source map support

--- tool/dist.sh
+++ tool/dist.sh
@@ -11,7 +11,9 @@ mkdir dist
 mkdir dist/src
 cp src/esl.js dist/src/esl.source.js
 # node tool/rm-ignore.js -i dist/src/esl.source.js -o dist/src/esl.source.min.js
-uglifyjs dist/src/esl.source.js -m -c -o dist/src/esl.js
+cd dist/src
+uglifyjs esl.source.js -m -c --source-map=esl.map --source-map-include-sources --source-map-url=esl.map -o esl.js
+cd ../../
 # uglifyjs dist/src/esl.source.min.js -m -c -o dist/src/esl.min.js
 uglifyjs src/js.js -m -c -o dist/src/js.js
 uglifyjs src/css.js -m -c -o dist/src/css.js
