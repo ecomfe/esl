dcd5b9ab27c60b316fba4a7bcfffd4dea1e63b16
leeight <leeight@gmail.com>
2017-08-10 13:17:38 +0800

    Fix #70

---
+++ test/bundles/issue70/zrender.js
@@ -0,0 +1,7 @@
+/**
+ * @file bundles/issue70/zrender.js
+ * @author leeight
+ */
+
+define('zrender', {name: 'zrender'});
+define('zrender/graphic/helper/smoothSpline', {name: 'zrender/graphic/helper/smoothSpline'});
