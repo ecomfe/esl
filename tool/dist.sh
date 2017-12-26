db07ee9429a38bcef5f1646442d4204bac8e099b
errorrik <errorrik@gmail.com>
2017-12-26 11:06:28 +0800

    dist script

--- tool/dist.sh
+++ tool/dist.sh
@@ -6,27 +6,31 @@ cd "$basedir/.."
 
 rm -rf dist
 mkdir dist
+rm -rf dist-check
+mkdir dist-check
 
 
-mkdir dist/src
-cp src/esl.js dist/src/esl.source.js
-# node tool/rm-ignore.js -i dist/src/esl.source.js -o dist/src/esl.source.min.js
-cd dist/src
+mkdir dist-check/src
+cp src/esl.js dist-check/src/esl.source.js
+# node tool/rm-ignore.js -i dist-check/src/esl.source.js -o dist-check/src/esl.source.min.js
+cd dist-check/src
 uglifyjs esl.source.js -m -c --source-map=esl.map --source-map-include-sources --source-map-url=esl.map -o esl.js
 cd ../../
-# uglifyjs dist/src/esl.source.min.js -m -c -o dist/src/esl.min.js
-uglifyjs src/js.js -m -c -o dist/src/js.js
-uglifyjs src/css.js -m -c -o dist/src/css.js
+# uglifyjs dist-check/src/esl.source.min.js -m -c -o dist-check/src/esl.min.js
+uglifyjs src/js.js -m -c -o dist-check/src/js.js
+uglifyjs src/css.js -m -c -o dist-check/src/css.js
 
-gzip -kf dist/src/esl.js
-# gzip -kf dist/src/esl.min.js
+gzip -kf dist-check/src/esl.js
+# gzip -kf dist-check/src/esl.min.js
 
 
-cp -r test dist/test
-# cp -r test dist/test-min
+cp -r test dist-check/test
+# cp -r test dist-check/test-min
 
-rm dist/test/impl/esl/esl.js
-cp dist/src/esl.js dist/test/impl/esl/esl.js
+rm dist-check/test/impl/esl/esl.js
+cp dist-check/src/esl.js dist-check/test/impl/esl/esl.js
 
-# rm dist/test-min/impl/esl/esl.js
-# cp dist/src/esl.min.js dist/test-min/impl/esl/esl.js
+# rm dist-check/test-min/impl/esl/esl.js
+# cp dist-check/src/esl.min.js dist-check/test-min/impl/esl/esl.js
+
+cp dist-check/src/*.js dist
